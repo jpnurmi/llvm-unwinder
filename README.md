1a3a32ad509d66d86835ee37b8c9532120cf0417
J-P Nurmi <jpnurmi@gmail.com>
2025-05-10 21:22:08 +0200

    libdw

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 ```sh
 $ cmake -B build -D USE_LLVM=OFF && cmake --build build && ./build/test-unwinder 
 [...]
-ip = 60b66c45a362, sp = 7ffdca6913c0, function = main+0x18
-ip = 77277962a338, sp = 7ffdca6913e0, function = __libc_init_first+0x88
-ip = 77277962a3fb, sp = 7ffdca691480, function = __libc_start_main+0x8b
-ip = 60b66c45a145, sp = 7ffdca6914e0, function = _start+0x25
+ip = 60fbf729c641, sp = 7fff27462390, proc = main+0x123, dw = main
+ip = 77704622a338, sp = 7fff274623c0, proc = __libc_init_first+0x88, dw = __libc_start_call_main
+ip = 77704622a3fb, sp = 7fff27462460, proc = __libc_start_main+0x8b, dw = __libc_start_main@@GLIBC_2.34
+ip = 60fbf729c285, sp = 7fff274624c0, proc = _start+0x25, dw = _start
 ```
 
 ## LLVM libunwind
@@ -16,8 +16,8 @@ ip = 60b66c45a145, sp = 7ffdca6914e0, function = _start+0x25
 ```sh
 $ cmake -B build -D USE_LLVM=ON && cmake --build build && ./build/test-unwinder 
 [...]
-ip = 5ab8cb68c4e2, sp = 7ffcd94af060, function = <unknown>
-ip = 7a28e382a338, sp = 7ffcd94af080, function = <unknown>
-ip = 7a28e382a3fb, sp = 7ffcd94af120, function = __libc_start_main+0x8b
-ip = 5ab8cb68c2c5, sp = 7ffcd94af180, function = <unknown>
+ip = 61171246d7a1, sp = 7fff32e9e970, proc = <unknown>, dw = main
+ip = 73e9d402a338, sp = 7fff32e9e9a0, proc = <unknown>, dw = __libc_start_call_main
+ip = 73e9d402a3fb, sp = 7fff32e9ea40, proc = __libc_start_main+0x8b, dw = __libc_start_main@@GLIBC_2.34
+ip = 61171246d3e5, sp = 7fff32e9eaa0, proc = <unknown>, dw = _start
 ```
--- main.c
+++ main.c
@@ -1,9 +1,19 @@
+#include <elfutils/libdw.h>
+#include <elfutils/libdwfl.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define UNW_LOCAL_ONLY
 #include <libunwind.h>
 
-void show_backtrace(void) {
+static const Dwfl_Callbacks dwfl_callbacks = {
+    .find_elf = dwfl_linux_proc_find_elf,
+    .find_debuginfo = dwfl_standard_find_debuginfo,
+};
+
+void show_backtrace(Dwfl *dwfl) {
   unw_cursor_t cursor;
   unw_context_t uc;
   unw_word_t ip, sp, offset;
@@ -11,19 +21,51 @@ void show_backtrace(void) {
 
   unw_getcontext(&uc);
   unw_init_local(&cursor, &uc);
+
   while (unw_step(&cursor) > 0) {
     unw_get_reg(&cursor, UNW_REG_IP, &ip);
     unw_get_reg(&cursor, UNW_REG_SP, &sp);
+
+    const char *name = NULL;
+    Dwfl_Module *module = dwfl_addrmodule(dwfl, ip);
+    if (module) {
+      GElf_Sym sym;
+      GElf_Off off;
+      name = dwfl_module_addrinfo(module, ip, &off, &sym, NULL, NULL, NULL);
+    }
+
     if (unw_get_proc_name(&cursor, sym, sizeof(sym), &offset) == 0) {
-      printf("ip = %lx, sp = %lx, function = %s+0x%lx\n", (long)ip, (long)sp,
-             sym, (long)offset);
+      printf("ip = %lx, sp = %lx, proc = %s+0x%lx, dw = %s\n", (long)ip,
+             (long)sp, sym, (long)offset, name);
     } else {
-      printf("ip = %lx, sp = %lx, function = <unknown>\n", (long)ip, (long)sp);
+      printf("ip = %lx, sp = %lx, proc = <unknown>, dw = %s\n", (long)ip,
+             (long)sp, name);
     }
   }
 }
 
 int main(int argc, char *argv[]) {
-  show_backtrace();
+  Dwfl *dwfl = dwfl_begin(&dwfl_callbacks);
+  if (!dwfl) {
+    fprintf(stderr, "dwfl_begin: %s\n", dwfl_errmsg(dwfl_errno()));
+    return -1;
+  }
+
+  if (dwfl_linux_proc_report(dwfl, getpid()) != 0) {
+    fprintf(stderr, "dwfl_linux_proc_report: %s\n", dwfl_errmsg(dwfl_errno()));
+    dwfl_end(dwfl);
+    return -1;
+  }
+
+  if (dwfl_report_end(dwfl, NULL, NULL) != 0) {
+    fprintf(stderr, "dwfl_report_end: %s\n", dwfl_errmsg(dwfl_errno()));
+    dwfl_end(dwfl);
+    return -1;
+  }
+
+  show_backtrace(dwfl);
+
+  dwfl_end(dwfl);
+
   return 0;
 }
