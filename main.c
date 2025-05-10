e4dd1f27916995c17543674459ef06ae0a90f4a3
J-P Nurmi <jpnurmi@gmail.com>
2025-05-10 20:35:12 +0200

    unw_get_proc_name

--- main.c
+++ main.c
@@ -6,14 +6,20 @@
 void show_backtrace(void) {
   unw_cursor_t cursor;
   unw_context_t uc;
-  unw_word_t ip, sp;
+  unw_word_t ip, sp, offset;
+  char sym[256];
 
   unw_getcontext(&uc);
   unw_init_local(&cursor, &uc);
   while (unw_step(&cursor) > 0) {
     unw_get_reg(&cursor, UNW_REG_IP, &ip);
     unw_get_reg(&cursor, UNW_REG_SP, &sp);
-    printf("ip = %lx, sp = %lx\n", (long)ip, (long)sp);
+    if (unw_get_proc_name(&cursor, sym, sizeof(sym), &offset) == 0) {
+      printf("ip = %lx, sp = %lx, function = %s+0x%lx\n", (long)ip, (long)sp,
+             sym, (long)offset);
+    } else {
+      printf("ip = %lx, sp = %lx, function = <unknown>\n", (long)ip, (long)sp);
+    }
   }
 }
 
